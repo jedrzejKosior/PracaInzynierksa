464d6b48bdabadede63695c85fda667bbb81a820
jedrzej.kosior <jedrzej.kosior@rightinformation.com>
2021-10-18 18:29:18 +0200

    'logowanie'

--- DHMS.py
+++ DHMS.py
@@ -1,63 +1,77 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 from kivy.lang import Builder
-from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.properties import StringProperty
 import psycopg2
 
-# #############################DATABASE START#############################
+# cur.execute("CREATE TABLE users(userName text,password text)")
+# cur.execute("INSERT INTO users VALUES('reception1', 'admin1234')")
 
-# connect to database
-conn = psycopg2.connect(host="localhost", database="hotel", user="postgres", password="admin")
-# connection = psycopg2.connect('hotel.db')
-
-# cursor
-cur = conn.cursor()
-
-# cur.execute("CREATE TABLE clients
-# (first_name text,last_name text,address text,city text,state text,zipcode text,room_number integer)")
-# cur.execute("INSERT INTO clients VALUES
-# ('Klaudia', 'Kromolowska', 'ul. Gminna 89', 'Czestochowa', 'Slask', '11-222', '5')")
-
-
-# commit your changes
-conn.commit()
-
-# close cursor
-cur.close()
-
-# close connection
-conn.close()
-
-# ################ DATABASE END #############
-
-# Name of style file
-Builder.load_file('login_page.kv')
 Window.minimum_width, Window.minimum_height = 500, 560
 
 
-class MyLayout(Widget):
+# Define our different screens
+class LoginWindow(Screen):
     login = ObjectProperty(None)
     password = ObjectProperty(None)
+    window = "loginWindow"
 
-    def press(self):
+    def loginPress(self):
+        # connect to database
+        conn = psycopg2.connect(host="localhost", database="hotel", user="postgres", password="admin")
+        # cursor
+        cur = conn.cursor()
+
         login = self.login.text
         password = self.password.text
-        print(f"Hello {login}, {password}")
+        cur.execute("SELECT * from users")
+        userData = cur.fetchall()
+        for user in userData:
+            if login == user[0]:
+                if password == user[1]:
+                    self.window = "mainWindow"
+                    break
+                else:
+                    # self.ids.loginLabel.text = f'SIKE!'
+                    print("Incorrect password")
+            else:
+                print("Incorrect login")
+
+        # print(f"Hello {login}, {password}")
+        # commit your changes
+        conn.commit()
+        # close cursor
+        cur.close()
+        # close connection
+        conn.close()
         # update button label
         # self.ids.printName.text = f'Hello {login}!'
         # Clear input
         self.login.text = ""
         self.password.text = ""
+        return self.window
 
 
-class Application(App):
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+# Name of style file
+kv = Builder.load_file('login_page.kv')
+
+
+class DesktopHotelManagementSystem(App):
+
     def build(self):
-        # background color
         Window.clearcolor = (206 / 255, 211 / 255, 220 / 255, 1)
-        return MyLayout()
+        return kv
 
 
 if __name__ == '__main__':
-    Application().run()
+    DesktopHotelManagementSystem().run()
