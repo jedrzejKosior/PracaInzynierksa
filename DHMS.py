39927bf1aca678d9f0e4519dbed882417286becb
jedrzej.kosior <jedrzej.kosior@rightinformation.com>
2021-11-22 19:41:51 +0100

    DateDesignEnded

--- DHMS.py
+++ DHMS.py
@@ -15,6 +15,55 @@ Window.minimum_width, Window.minimum_height = 500, 580
 
 
 # validations of inputs
+
+# def is_already_taken_for_update(room_number, starting_date, ending_date, client):
+#     conn = psycopg2.connect('hotel.db')
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT book_start, book_end, client_id FROM room" + str(room_number))
+#     status_checking = cursor.fetchall()
+#
+#     # commit changes
+#     conn.commit()
+#
+#     # close connection
+#     conn.close()
+#     i = 0
+#     # for i in range(len(status_checking)):
+#     while (i < len(status_checking)):
+#         if ((starting_date >= status_checking[i][0] and starting_date < status_checking[i][1]) or (
+#                 ending_date > status_checking[i][0] and ending_date <= status_checking[i][1]) or (
+#                 starting_date <= status_checking[i][0] and ending_date >= status_checking[i][1])):
+#             if (int(client) == int(status_checking[i][2])):
+#                 i = i + 1
+#                 continue
+#             else:
+#                 return True
+#         i = i + 1
+#     return False
+#
+#
+# def is_already_taken(room_number, starting_date, ending_date):
+#     conn = psycopg2.connect('hotel.db')
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT book_start, book_end, client_id FROM room" + str(room_number))
+#     status_checking = cursor.fetchall()
+#
+#     # commit changes
+#     conn.commit()
+#
+#     # close connection
+#     conn.close()
+#     i = 0
+#     # for i in range(len(status_checking)):
+#     while (i < len(status_checking)):
+#         if ((starting_date >= status_checking[i][0] and starting_date < status_checking[i][1]) or (
+#                 ending_date > status_checking[i][0] and ending_date <= status_checking[i][1]) or (
+#                 starting_date <= status_checking[i][0] and ending_date >= status_checking[i][1])):
+#             return True
+#         i = i + 1
+#     return False
+
+
 def is_not_right_zipcode(textInput):
     if len(textInput) == 0 or len(textInput) != 6:
         return True
@@ -90,6 +139,11 @@ class LoginWindow(Screen):
 class DateWindow(Screen):
     startDay = ObjectProperty(None)
     endDay = ObjectProperty(None)
+    startMonth = ObjectProperty(None)
+    endMonth = ObjectProperty(None)
+    startYear = ObjectProperty(None)
+    endYear = ObjectProperty(None)
+
     months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER",
               "NOVEMBER", "DECEMBER"]
 
@@ -98,21 +152,21 @@ class DateWindow(Screen):
     currentDay = str(datetime.now().day)
     daysStart = []
     for i in range(monthrange(int(currentYear), currentMonth)[1]):
-        daysStart.append(str(i+1))
+        daysStart.append(str(i + 1))
     daysEnd = daysStart.copy()
 
     def daysInSelectedMonthForStart(self):
         self.daysStart = []
-        for i in range(monthrange(int(self.ids.startYear.text), self.months.index(self.ids.startMonth.text)+1)[1]):
-            self.daysStart.append(str(i+1))
+        for i in range(monthrange(int(self.ids.startYear.text), self.months.index(self.ids.startMonth.text) + 1)[1]):
+            self.daysStart.append(str(i + 1))
         self.startDay.values = self.daysStart
         if int(self.startDay.text) > int(self.daysStart[-1]):
             self.startDay.text = str(self.daysStart[-1])
 
     def daysInSelectedMonthForEnd(self):
         self.daysEnd = []
-        for i in range(monthrange(int(self.ids.endYear.text), self.months.index(self.ids.endMonth.text)+1)[1]):
-            self.daysEnd.append(str(i+1))
+        for i in range(monthrange(int(self.ids.endYear.text), self.months.index(self.ids.endMonth.text) + 1)[1]):
+            self.daysEnd.append(str(i + 1))
         self.endDay.values = self.daysEnd
         if int(self.endDay.text) > int(self.daysEnd[-1]):
             self.endDay.text = str(self.daysEnd[-1])
@@ -120,6 +174,19 @@ class DateWindow(Screen):
     def limit_spinner(self):
         maxItems = 3
         self.spinner.dropdown_cls.max_height = maxItems * dp(48)
+
+    def wrongTimePeriod(self):
+        isCorrect = True
+        if int(self.ids.startYear.text) > int(self.ids.endYear.text):
+            isCorrect = False
+        if (self.months.index(self.ids.startMonth.text) > self.months.index(self.ids.endMonth.text)) and (
+                int(self.ids.startYear.text) >= int(self.ids.endYear.text)):
+            isCorrect = False
+        if (int(self.ids.startDay.text) >= int(self.ids.endDay.text)) and (
+                self.months.index(self.ids.startMonth.text) >= self.months.index(self.ids.endMonth.text)) and (
+                int(self.ids.startYear.text) >= int(self.ids.endYear.text)):
+            isCorrect = False
+        return isCorrect
 
     def searchPress(self):
         pass
